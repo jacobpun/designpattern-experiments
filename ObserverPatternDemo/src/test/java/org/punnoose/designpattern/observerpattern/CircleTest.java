b5bc81c912fae48f3d1c58ee8db142ed553d6448
Punnoose <punnoosekuttyj@yahoo.com>
2014-08-03 15:27:40 -0400

    cosmetic changes

--- ObserverPatternDemo/src/test/java/org/punnoose/designpattern/observerpattern/CircleTest.java
+++ ObserverPatternDemo/src/test/java/org/punnoose/designpattern/observerpattern/CircleTest.java
@@ -8,12 +8,16 @@ import org.junit.Test;
 public class CircleTest {
 
 	@Test
-	public void test() {
+	public void testObserver() {
+		final int X_AXIS = 10;
+		final int Y_AXIS = 20;
+		final int RADIUS = 30;
+		final int NEW_RADIUS = 50;
+
 		ShapeAudit audit = new ShapeAudit();
-		Circle circle = new Circle(10,20,30);
+		Circle circle = new Circle(X_AXIS,Y_AXIS,RADIUS);
 		circle.addObserver(audit);
-		circle.setRadius(50);
-		circle.setCenter(new Point2D(5,5));
-		assertThat(audit.getauditMessages(), hasItem("Radius changed to 50"));
+		circle.setRadius(NEW_RADIUS);
+		assertThat(audit.getauditMessages(), hasItem("Radius changed to " + NEW_RADIUS));
 	}
 }
